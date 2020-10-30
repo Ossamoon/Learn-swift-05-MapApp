f085bef6e6b127ec9687095760833e4b4752899b
Ossamoon <osamu950623@gmail.com>
2020-10-30 11:11:17 +0900

    書籍に従い完成させた

---
+++ MapApp/MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by 齋藤修 on 2020/10/30.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
